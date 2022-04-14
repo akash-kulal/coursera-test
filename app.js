c78f088e0be937f888f4878159420c94e2acec65
Poojashree Kulal <21ca03.akash@sjec.ac.in>
2022-04-14 09:58:51 +0530

    files

---
+++ app.js
@@ -0,0 +1,21 @@
+(function (){
+  'use strict';
+
+angular.module('CounterApp', [])
+  .controller('CounterController',CounterController);
+
+  CounterController.$inject = ['$scope'];
+  function CounterController($scope) {
+    $scope.onceCounter = 0;
+
+    $scope.showNumberOfWatchers = function() {
+      console.log("# of Watchers: ", $scope.$$WatchersCount);
+    };
+
+    $scope.countOnce = function () {
+      $scope.onceCounter = 1;
+    }
+  }
+
+
+})();
